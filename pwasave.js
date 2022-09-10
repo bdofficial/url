ffd18a047c6b8ae03ac64111dbadddd1ca618747
banglaploa@gmail.com <banglaploa@gmail.com>
2022-09-10 11:32:30 +0600

    555

---
+++ pwasave.js
@@ -0,0 +1,31 @@
+//delete cache
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keyList) => {
+    Promise.all(keyList.map((key) => {
+      if (key === cacheName) { return; }
+      caches.delete(key);
+    }))
+  }));
+});
+var cacheName = 'dj—alok';
+//save cache
+var filesToCache = [
+  "/",
+  ];
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
